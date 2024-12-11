5b1a9f2eba21fdd7924b91cb05e156fe12bd5418
Moazam05 <salmanmoazam08@gmail.com>
2024-12-11 16:57:47 +0500

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    // "@nuxtjs/supabase",
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK_KEY,
+    },
+  },
+  app: {
+    head: {
+      script: [{ src: "https://js.stripe.com/v3/", defer: true }],
+    },
+  },
+});
