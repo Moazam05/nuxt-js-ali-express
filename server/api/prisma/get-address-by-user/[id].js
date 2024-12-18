f901d9bcfec911f475ecfc79eb72d75ca9a105a1
Moazam05 <salmanmoazam08@gmail.com>
2024-12-18 17:42:03 +0500

    id set

--- server/api/prisma/get-address-by-user/[id].js
+++ server/api/prisma/get-address-by-user/[id].js
@@ -2,13 +2,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const id = event.context.params.id;
-
-  const address = await prisma.addresses.findFirst({
-    where: {
-      userId: Number(id),
-    },
+  const res = await prisma.addresses.findFirst({
+    where: { userId: event.context.params.id },
   });
-
-  return address;
+  return res;
 });
